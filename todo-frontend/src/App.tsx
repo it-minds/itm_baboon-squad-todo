3505d92a605c4f46078c6607e8cd8df6a8b2afc5
Nicolai Bak <nba@it-minds.dk>
2022-09-05 10:32:55 +0200

    Adding tailwind to frontend + gitignore to backend

--- todo-frontend/src/App.tsx
+++ todo-frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import './index.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div >
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -15,17 +16,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold">Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className="bg-black text-black" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and s  ave to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="text-3xl">
+        Click on the Vite gfsgdfg React logos to learn more
       </p>
     </div>
   )
