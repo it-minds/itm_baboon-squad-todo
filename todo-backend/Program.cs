9658fb25f220f85b191b40b472b5cff79696a3ab
AntonEsbjerg <202008162@post.au.dk>
2022-09-07 12:55:32 +0200

    Localhost 5174 added

--- todo-backend/Program.cs
+++ todo-backend/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: "MyPolicy",
                 policy =>
                 {
-                    policy.WithOrigins("http://localhost:5173");
+                    policy.WithOrigins("http://localhost:5173", "http://localhost:5174");
                 });
 });
 
