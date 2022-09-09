18515eeefe52f63d60864e65a385e42426d0ac87
AntonEsbjerg <202008162@post.au.dk>
2022-09-09 10:45:36 +0200

    New local host

--- todo-backend/Program.cs
+++ todo-backend/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: "MyPolicy",
                 policy =>
                 {
-                    policy.WithOrigins("http://localhost:5173", "http://localhost:5175")
+                    policy.WithOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:5175")
                                         .AllowAnyHeader()
                                         .AllowAnyMethod();
                 });
