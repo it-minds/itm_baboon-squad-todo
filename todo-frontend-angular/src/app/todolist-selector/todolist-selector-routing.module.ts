8d69bc5d8a5d6ac5c42f523745c33814ff5214b1
Nicolai Bak <nba@it-minds.dk>
2022-09-13 13:21:23 +0200

    updating todo to todolist-selector

--- todo-frontend-angular/src/app/todolist-selector/todolist-selector-routing.module.ts
+++ todo-frontend-angular/src/app/todolist-selector/todolist-selector-routing.module.ts
@@ -8,4 +8,4 @@ const routes: Routes = [{ path: '', component: TodolistSelectorComponent }];
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class TodoRoutingModule { }
+export class TodolistSelectorRoutingModule { }
