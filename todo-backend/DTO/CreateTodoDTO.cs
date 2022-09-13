44da03df5f1f363438253ec60a7dd60c19a556b3
Nicolai Bak <nba@it-minds.dk>
2022-09-13 10:20:06 +0200

    nullable warnings in data models disabled

--- todo-backend/DTO/CreateTodoDTO.cs
+++ todo-backend/DTO/CreateTodoDTO.cs
@@ -2,9 +2,12 @@
 {
     public class CreateTodoDTO
     {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
         public string Title { get; set; }
         public DateTime Deadline { get; set; }
         public int TodoListId { get; set; }
         public int Position { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     }
 }
