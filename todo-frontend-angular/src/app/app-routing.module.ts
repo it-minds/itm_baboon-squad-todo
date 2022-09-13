4c332f6ec115a9e30760df49572fdd9f3b7572db
Nicolai Bak <nba@it-minds.dk>
2022-09-13 13:21:23 +0200

    updating todo to todolist-selector

--- todo-frontend-angular/src/app/app-routing.module.ts
+++ todo-frontend-angular/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'Todo', loadChildren: () => import('./todolist-selector/todolist-selector.module').then(m => m.TodoModule) }
+const routes: Routes = [{ path: 'Todo', loadChildren: () => import('./todolist-selector/todolist-selector.module').then(m => m.TodolistSelectorModule) }
 ];
 
 @NgModule({
--- todo-frontend-angular/src/app/todolist-selector/todolist-selector.component.spec.ts
+++ todo-frontend-angular/src/app/todolist-selector/todolist-selector.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodolistSelectorComponent } from './todolist-selector.component';
 
-describe('TodoComponent', () => {
+describe('TodolistSelectorComponent', () => {
   let component: TodolistSelectorComponent;
   let fixture: ComponentFixture<TodolistSelectorComponent>;
 
