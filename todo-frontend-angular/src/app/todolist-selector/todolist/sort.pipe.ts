d953b54b1a3b5b56256379b0ca82d27db1ef3df4
Nicolai Bak <nba@it-minds.dk>
2022-09-14 12:53:07 +0200

    todos binded to service property (132), sorting updated to match property type

--- todo-frontend-angular/src/app/todolist-selector/todolist/sort.pipe.ts
+++ todo-frontend-angular/src/app/todolist-selector/todolist/sort.pipe.ts
@@ -1,11 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Todo } from 'src/app/models/todo.model';
 
 @Pipe({
   name: 'sort'
 })
 
 export class SortPipe implements PipeTransform {
-  transform(todos: any[], field: string): any[] {
-    return todos.sort((a: any, b: any) => a[field] - b[field]);
+  transform(todos: any[] | null, field: string): any[] {
+    return todos ? todos.sort((a: any, b: any) => a[field] - b[field]) : [];
   }
 }
