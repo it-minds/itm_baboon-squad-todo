15aad62d399dbb2a3a1514c8f78ba1a1ab763fba
Nicolai Bak <nba@it-minds.dk>
2022-09-16 09:24:48 +0200

    add todolist modal added, missing service call to api

--- todo-frontend-angular/src/app/todolist-selector/todolist-selector.module.ts
+++ todo-frontend-angular/src/app/todolist-selector/todolist-selector.module.ts
@@ -9,14 +9,18 @@ import { TodoComponent } from './todo/todo.component';
 import { NzModalModule } from 'ng-zorro-antd/modal';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { AddTodoModalComponent } from './add-todo-modal/add-todo-modal.component';
 import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ToUppercasePipe } from './todo/to-uppercase.pipe';
 import { SortPipe } from './todolist/sort.pipe';
 import { SubtaskComponent } from './subtask/subtask.component';
 import { RemoveDuplicatesPipe } from './remove-duplicates.pipe';
+import { AddTodolistModalComponent } from './add-todolist-modal/add-todolist-modal.component';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzModalModule } from 'ng-zorro-antd/modal';
 
 
 @NgModule({
@@ -30,6 +34,7 @@ import { RemoveDuplicatesPipe } from './remove-duplicates.pipe';
     SortPipe,
     SubtaskComponent,
     RemoveDuplicatesPipe,
+    AddTodolistModalComponent,
   ],
   imports: [
     CommonModule,
