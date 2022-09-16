e1e48fb536855fb47ddcb6ec72c346df9f8cbe13
Nicolai Bak <nba@it-minds.dk>
2022-09-16 09:24:48 +0200

    add todolist modal added, missing service call to api

---
+++ todo-frontend-angular/src/app/todolist-selector/add-todolist-modal/add-todolist-modal.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-todolist-modal',
+  templateUrl: './add-todolist-modal.component.html',
+  styleUrls: ['./add-todolist-modal.component.scss']
+})
+export class AddTodolistModalComponent implements OnInit {
+  isVisible = false;
+  isOkLoading = false;
+  validateForm = new FormGroup({
+    title: new FormControl<string>("", [Validators.required, Validators.minLength(1)]),
+  })
+  constructor() { }
+
+  @Output() addTodoSubmit = new EventEmitter<string>();
+
+  ngOnInit(): void {
+  }
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  handleSubmit(): void {
+    if (this.validateForm.valid) {
+      this.isOkLoading = true;
+      const newTodolistTitle: string = this.validateForm.value.title!;
+      this.addTodoSubmit.emit(newTodolistTitle)
+      this.validateForm.reset()
+      this.isVisible = false;
+      this.isOkLoading = false;
+    }
+  }
+
+  handleCancel(): void {
+    this.isVisible = false;
+    this.validateForm.reset()
+  }
+}
--- todo-frontend-angular/src/app/todolist-selector/todolist-selector.module.ts
+++ todo-frontend-angular/src/app/todolist-selector/todolist-selector.module.ts
@@ -9,11 +9,14 @@ import { IconsProviderModule } from '../icons-provider.module';
 import { TodoComponent } from './todo/todo.component';
 import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 import { NzGridModule } from 'ng-zorro-antd/grid';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ToUppercasePipe } from './todo/to-uppercase.pipe';
 import { SortPipe } from './todolist/sort.pipe';
 import { SubtaskComponent } from './subtask/subtask.component';
 import { RemoveDuplicatesPipe } from './remove-duplicates.pipe';
+import { AddTodolistModalComponent } from './add-todolist-modal/add-todolist-modal.component';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzModalModule } from 'ng-zorro-antd/modal';
 
 
 
@@ -27,6 +30,7 @@ import { RemoveDuplicatesPipe } from './remove-duplicates.pipe';
     SortPipe,
     SubtaskComponent,
     RemoveDuplicatesPipe,
+    AddTodolistModalComponent,
   ],
   imports: [
     CommonModule,
@@ -34,7 +38,9 @@ import { RemoveDuplicatesPipe } from './remove-duplicates.pipe';
     IconsProviderModule,
     NzCheckboxModule,
     NzGridModule,
-    FormsModule
+    FormsModule,
+    NzModalModule,
+    NzFormModule,
   ]
 })
 export class TodolistSelectorModule { }
