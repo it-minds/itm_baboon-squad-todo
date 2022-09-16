b6456ffce229af8c56e1c04bb060a32112b2b668
Nicolai Bak <nba@it-minds.dk>
2022-09-16 10:23:46 +0200

    on todolist select updated to clear todos before fetching

--- todo-frontend-angular/src/app/todolist-selector/todolist/todolist.component.ts
+++ todo-frontend-angular/src/app/todolist-selector/todolist/todolist.component.ts
@@ -13,12 +13,11 @@ import { Observable } from 'rxjs';
 })
 
 export class TodolistComponent implements OnInit {
-  // todos: Todo[] = [];
   todoLists: Todolist[] = [];
   selectedValue: Todolist | null = null;
   todos$: Observable<Todo[]> = this.todoService.todos$
-  todolistIds: string[] = []
   showSubtasksOnTodoId: number | null = null
+
   moreBtnConfig: ButtonConfiguration = {
     styles: {
       position: 'relative',
@@ -55,37 +54,20 @@ export class TodolistComponent implements OnInit {
   onDoubleClick(todo: Todo) {
     this.showSubtasksOnTodoId = this.showSubtasksOnTodoId === todo.todoListId ? null : todo.todoId
   }
-
-  onClickEventReceived() {
-  }
-
   onCheckboxClick(isChecked: boolean, todo: Todo) {
     this.todoService.updateTodo({
       ...todo, checked: isChecked
-    }).subscribe({
-      // next: () => {
-      //   this.todoService.clearTodos()
-      //   this.todolistIds.forEach(todolistId => {
-      //     this.todoService.getTodos(todolistId)
-      //   })
-      // }
-    })
+    }).subscribe()
   }
   onTodolistSelect(value: Todolist) {
+    this.todoService.clearTodos()
+    this.selectedValue = value;
     if (value != null) {
       this.todoLists.filter(todolist => todolist.title === value.title)
         .forEach(todolist => {
-          this.todolistIds.push(todolist.todoListId.toString())
-        })
-
-      this.todolistIds.forEach(todolistId => {
-        this.todoService.getTodos(todolistId)
-      });
+          this.todoService.getTodos(todolist.todoListId.toString())
+        });
     }
-    else {
-      this.todoService.clearTodos()
-    }
-    this.selectedValue = value;
   }
   onAddTodoSubmit(newTodo: NewTodoDTO) {
     this.todoService.addTodo(newTodo).subscribe({ next: () => this.onTodolistSelect(this.selectedValue!) })
