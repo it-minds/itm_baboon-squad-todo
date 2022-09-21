d7b13fb63f9f83888e713175f60147a5f071b411
Nicolai Bak <nba@it-minds.dk>
2022-09-21 09:53:34 +0200

    test prj added

---
+++ todo-backend/todo-backend.test.unit/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace todo_backend.test.unit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
