40900e9f21259229e7f9f57a0099b5367fd284c4
Nicolai Bak <nba@it-minds.dk>
2022-09-21 12:48:17 +0200

    test added

--- todo-backend/todo-backend.test.unit/UnitTest1.cs
+++ todo-backend/todo-backend.test.unit/UnitTest1.cs
@@ -1,10 +1,44 @@
+using todo_backend.Classes;
+using todo_backend.Interfaces;
+using todo_backend.Logic;
+
 namespace todo_backend.test.unit;
 
 public class Tests
 {
+    private readonly ITodoArranger _uut = new TodoArranger();
     [SetUp]
+
     public void Setup()
     {
+    }
+
+    [Test]
+    public void ArrangePositionAfterDelete_No_Error()
+    {
+        // Arrange
+        var todo1 = new Todo()
+        {
+            Position = 1
+        };
+        var todo2 = new Todo()
+        {
+            Position = 2
+        };
+        var todo3 = new Todo()
+        {
+            Position = 3
+        };
+        var todolist = new TodoList()
+        {
+            Todos = new List<Todo>() { todo1, todo2 }
+        };
+
+        // Act
+        todolist = _uut.ArrangePositionAfterDelete(todolist, todo3);
+
+        // Assert
+        Assert.That(todolist.Todos[0].Position == 1);
     }
 
     [Test]
